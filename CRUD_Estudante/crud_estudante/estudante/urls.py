ebc870db899305308f9140282c6f5a42e7b86450
Alyson Pereira dos Santos <53673071831@>
2025-03-13 16:42:36 -0300

    Uploading File 13/03

--- CRUD_Estudante/crud_estudante/estudante/urls.py
+++ CRUD_Estudante/crud_estudante/estudante/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('alunos/', views.listar_alunos),
-    path('alunos/criar/', views.criar_aluno)
+    path('alunos/<int:pk>/', views.detalhe_aluno),
+    path('  ', views.criar_aluno),
+    path('alunos/alterar/<int:pk>/', views.alterar_aluno),
+    path('alunos/apagar/<int:pk>/', views.deletar_informacoes),
+    path('fazAlgo/<str:text>', views.macharete),
 ]
+
--- CRUD_Estudante/crud_estudante/estudante/views.py
+++ CRUD_Estudante/crud_estudante/estudante/views.py
@@ -13,6 +13,17 @@ def listar_alunos(request):
     serializer = AlunoSerializer(alunos, many=True)
     return Response(serializer.data)
 
+
+@api_view(['GET'])
+def detalhe_aluno(request, pk):
+    try:
+        aluno = Aluno.objects.get(pk=pk)
+    except Aluno.DoesNotExist:
+        return Response("{'Erro': 'O aluno não foi Encontrado''}", status=status.HTTP_404_NOT_FOUND)
+    serializer = AlunoSerializer(aluno)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
 @api_view(['POST'])
 def criar_aluno(request):
     if request.method == 'POST':
@@ -21,3 +32,33 @@ def criar_aluno(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+@api_view(['PUT'])
+def alterar_aluno(request, pk):
+    try:
+        aluno = Aluno.objects.get(pk=pk)
+    except Aluno.DoesNotExist:
+        return Response("{'Erro': 'O aluno não foi encontrado'}", status=status.HTTP_404_NOT_FOUND)
+    serializer = AlunoSerializer(aluno, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def deletar_informacoes(request, pk):
+    try:
+        aluno = Aluno.objects.get(pk=pk)
+    except Aluno.DoesNotExist:
+        return Response("{'Erro': 'Informações não encontrado'}", status=status.HTTP_404_NOT_FOUND)
+    aluno.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
+def macharete(request, text):
+    import pyfiglet
+    gabriel = pyfiglet.figlet_format(text)
+
+    return Response(gabriel, status=status.HTTP_200_OK)

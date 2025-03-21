da4cc7502fcea7c25b2dbf2a6aaf4a66f33ac8bf
skittlexyz <mo644774@gmail.com>
2025-03-21 09:03:41 -0300

    Uploading file 21/03

--- 4- Biblioteca - BOSCH/meu_crud/crud/models.py
+++ 4- Biblioteca - BOSCH/meu_crud/crud/models.py
@@ -6,7 +6,7 @@ class Livros(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
     titulo = models.CharField(max_length=255)
     autor = models.CharField(max_length=255)
-    ano_publicacao = models.DateTimeField(auto_now=True)
+    ano_publicacao = models.DateTimeField(auto_created=True)
     descricao = models.CharField(max_length=500)
     data_criacao = models.DateTimeField(auto_now_add=True)
 
--- 4- Biblioteca - BOSCH/meu_crud/crud/urls.py
+++ 4- Biblioteca - BOSCH/meu_crud/crud/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns = [
     path('', views.lista_crud, name='lista_crud')
 
+
 ]

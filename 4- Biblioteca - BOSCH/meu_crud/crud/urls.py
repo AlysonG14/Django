fc7ad7931ead26627a3636e4730889e5094ebb2f
Alyson Pereira <alyson.bosch.senai14@gmail.com>
2025-03-23 21:56:19 -0300

    Uploading File 23/03

--- 4- Biblioteca - BOSCH/meu_crud/crud/urls.py
+++ 4- Biblioteca - BOSCH/meu_crud/crud/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.lista_crud, name='lista_crud')
+    path('crud/', views.lista_crud, name='lista_crud.html'),
+    path('crud/criar/', views.book_create, name='criar.html'),
+    path('crud/<int:pk>', views.book_views),
+    path('crud/atualizar/<int:pk>', views.book_update, name='atualizar.html'),
+    path('crud/deletar/<int:pk>', views.delete_book, name='delete.html'),
+    path('crud/busca/', views.search_book, name='busca.html')
 
 
 ]
--- 4- Biblioteca - BOSCH/meu_crud/crud/views.py
+++ 4- Biblioteca - BOSCH/meu_crud/crud/views.py
@@ -8,6 +8,7 @@ def lista_crud(request):
     lista_crud = Livros.objects.all()
     return render(request, 'crud/lista_crud.html', {'lista_crud' : lista_crud})
 
+
 def book_create(request):
     if request.method == "POST":
         form = LivrosForm(request.POST)
@@ -16,14 +17,13 @@ def book_create(request):
         return redirect ('lista_crud')
     else:
         form = LivrosForm()
-
     return render(request, 'crud/criar.html', {'form': form})
 
-def book_views(request, pk):
-    if request.method == "POST":
-        book = get_object_or_404(Livros, pk=pk)
 
-    return render(request, 'crud/detalhes_produtos.html', {'book' : book})
+def book_views(request, pk):
+        book = get_object_or_404(Livros, pk=pk)
+        return render(request, 'crud/book_views.html', {'book' : book})
+
 
 def book_update(request, pk):
     book = get_object_or_404(Livros, pk=pk)
@@ -31,10 +31,11 @@ def book_update(request, pk):
         form = LivrosForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
-        return redirect('lista_crud')
+        return redirect('book_views', pk=book.pk)
     else:
-        form = LivrosForm()
-    return render(request, 'crud/criar.html', {'form': form})
+        form = LivrosForm(instance=book)
+    return render(request, 'crud/atualizar.html', {'form': form})
+
 
 def delete_book(request, pk):
     book = get_object_or_404(Livros, pk=pk)
@@ -42,3 +43,22 @@ def delete_book(request, pk):
         book.delete()
         return redirect('lista_crud')
     return render(request, 'crud/delete.html', {'book': book})
+
+def search_book(request):
+    # Captura os parâmetros da URL
+    titulo = request.GET.get('nome', '')
+    autor = request.GET.get('autor', '')
+    ano = request.GET.get('ano', '')
+
+    # Filtra os livros com base nos parâmetros, ignorando se o parâmetro está vazio
+    livros = Livros.objects.all()
+
+    if titulo:
+        livros = Livros.objects.filter(titulo__icontains=titulo) # 'icontains' Começa a fazer uma busca sem o Case Sensitive
+    if autor:
+        livros = Livros.objects.filter(autor__icontains=autor)
+    if ano:
+        livros = Livros.objects.filter(ano_publicacao=ano)
+
+    return render(request, 'crud/busca.html', {'livros' : livros})
+

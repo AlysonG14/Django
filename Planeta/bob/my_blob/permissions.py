878a675318b577cabcfab90df8f7aad5e047e361
Alyson Pereira dos Santos <53673071831@>
2025-04-24 16:57:17 -0300

    Uploading File 24/04

---
+++ Planeta/bob/my_blob/permissions.py
@@ -0,0 +1,19 @@
+from rest_framework.permissions import BasePermission
+
+class IsHumano(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.especie == 'H'
+    
+class IsAlien(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.especie == 'A'
+    
+class IsExperimento(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.especie == 'E'
+    
+class IsHumanoOuMorador(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user.especie == 'H':
+            return True
+        return obj.id == request.user.planeta.id
